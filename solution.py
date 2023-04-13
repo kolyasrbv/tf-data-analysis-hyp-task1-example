0bfb9c0e12f67606aef455fd237816a8b8db1b18
Nikolay Serebryakov <116488084+kolyasrbv@users.noreply.github.com>
2023-04-13 14:55:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+import scipy.stats as st
 
 chat_id = 433193277 # Ваш chat ID, не меняйте название переменной
 
@@ -8,7 +8,12 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    if(x_cnt / y_cnt < x_success / y_success):
-        return True
-    return False
-solution(x_success, x_cnt, y_success, y_cnt)
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    x = x_success / x_cnt
+    y = y_success / y_cnt
+    std_diff = np.sqrt((x * (1 - x) / x_cnt) + (y * (1 - y) / y_cnt))
+    t = np.abs(y - x) / std_diff
+    c = st.norm.ppf(1 - 0.05/2)
+    return t > c
